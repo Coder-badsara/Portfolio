2fbe575ba2b85794a3993fc58c0c00bbf898776f
UMESH BADSARA <umesh2505badsara@gmail.com>
2026-08-24 18:20:41 +0530

    Add Vercel Analytics component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
@@ -8,6 +9,7 @@ export default function App({ Component, pageProps }) {
         <title>Umesh Badsara - Backend Developer Portfolio</title>
       </Head>
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
